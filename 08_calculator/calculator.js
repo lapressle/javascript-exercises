eeb9f3eb0deb10fa3af2d8ec32af229e115db5f5
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 17:31:55 -0500

    add calculator fcts

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1,number2) {
+  return number1-number2;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+  let adding = 0;
+  for (const number of numbers) {
+    for (const i of number) {
+      adding += i;
+    }
+  }
+  return adding;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let multiplying = 1;
+  for (const number of numbers) {
+    for (const i of number) {
+      multiplying = multiplying * i;
+    }
+  }
+  return multiplying;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1**number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+      let multiplying = number
+      for (let i=number-1; i>0; i--) {
+        multiplying = i * multiplying;
+    }
+    return multiplying
+  }
+
 };
 
 // Do not edit below this line
