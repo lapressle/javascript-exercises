ebdfbc2e9d724eedd99b68e656f316ffaf44ce3b
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 16:29:09 -0500

    add function for removeArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeValue) {
+    filteredArray = [];
+loop1:   for (const value of arr) {
+    loop2:    for (const arg of removeValue) {
+                    if (value === arg) {
+                        continue loop1;
+                    }
+            }
+            filteredArray.push(value)
+        }
+    return filteredArray;   
 };
 
 // Do not edit below this line
