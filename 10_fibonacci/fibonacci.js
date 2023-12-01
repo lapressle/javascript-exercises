bee97ce2fa79b795a4cdd6e8bcea5b57aba306f1
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 18:09:51 -0500

    add function for fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return 'OOPS';
+    } else {
+        let number1 = 0;
+        let number2 =1
+        let n = 1;
+        while (n < Number(number)) {
+            i = number1 + number2;
+            number1 = number2;
+            number2 = i;
+            n += 1;
+        }
+        return number2
+    }
 };
 
 // Do not edit below this line
