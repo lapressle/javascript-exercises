ed28b37c784a71e8c6282f6ed6d119e16442de24
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 15:40:49 -0500

    add function for soln to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    const splitWord = word.split(' ');
+    const splitString = [];
+    const reverse = [];
+    const words = [];
+
+    for (let i = 0; i<splitWord.length; i++) {
+        splitString.push(splitWord[i].split(''));
+        reverse.push(splitString[i].reverse());
+        words.push(reverse[i].join(''));
+    }
+    const reversedWords = words.reverse();
+    return reversedWords.join(' ');
 };
 
 // Do not edit below this line
