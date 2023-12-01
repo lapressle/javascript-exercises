cc82508dead6e9fc53b0cd983b14f74650340fc8
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 16:43:04 -0500

    add ability to avoid poor variables
    This function now avoids negative numbers and non number types.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,16 @@
 const sumAll = function(number1, number2) {
     let adding = 0;
-    for (let i = Math.min(number1, number2); i <= Math.max(number1, number2); i++) {
-        adding += i;
+    if (typeof number1 != 'number' || typeof number2 != 'number') {
+        return 'ERROR';
     }
-    return adding;
+    else if (number1 < 0 || number2 < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = Math.min(number1, number2); i <= Math.max(number1, number2); i++) {
+            adding += i;
+        }
+        return adding;
+    }
 };
 
 // Do not edit below this line
