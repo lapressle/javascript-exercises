3ce2d6f21862044f680be2d4f6363448ed6fcb93
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 17:56:18 -0500

    add function for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const regex = /[!"#$%&, '()*+-./:;<=>?@[\]^_`{|}~]/g;
+    word = word.replace(regex,'');
+    word = word.toLowerCase();
+    const splitWord = word.split(' ');
+    const splitString = [];
+    const reverse = [];
+    const words = [];
 
-};
+    for (let i = 0; i<splitWord.length; i++) {
+        splitString.push(splitWord[i].split(''));
+        reverse.push(splitString[i].reverse());
+        words.push(reverse[i].join(''));
+    }
+    const reversedWords = words.reverse();
+    if (reversedWords.join('') == word) {
+        console.log('help')
+        return true;
+    }   else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
