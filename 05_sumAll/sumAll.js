aa4eaa7ae38dc5778869d19d0443728ac1d041de
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-01 16:37:46 -0500

    add function for sumAll
    function currently sums from the smaller of two numbers to the
    larger but does not work with negatives

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let adding = 0;
+    for (let i = Math.min(number1, number2); i <= Math.max(number1, number2); i++) {
+        adding += i;
+    }
+    return adding;
 };
 
 // Do not edit below this line
